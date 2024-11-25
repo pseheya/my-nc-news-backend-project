f8b9f94edd0c24c90e762a42ab1cb12588651120
Oksana Herasymenko <oksanagerasymenko@Mac.mynet>
2024-11-25 16:55:33 +0000

    task 6 is completed and test with errors completed

--- models/app.model.js
+++ models/app.model.js
@@ -34,3 +34,14 @@ exports.readArticles = () => {
     return rows;
   });
 };
+
+exports.readCommentsByArticleId = (id) => {
+  return db
+    .query("SELECT * FROM comments WHERE article_id = $1", [id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not found" });
+      }
+      return rows;
+    });
+};
