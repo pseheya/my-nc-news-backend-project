09ce2ea858697e6dce7f52c63d6fa964634aa836
Oksana Herasymenko <oksanagerasymenko@Mac.mynet>
2024-11-26 16:51:37 +0000

    task 13 completed with test

--- models/app.model.js
+++ models/app.model.js
@@ -13,7 +13,23 @@ exports.readArticleByID = (id) => {
 
   return db
     .query(
-      "SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url FROM articles WHERE article_id = $1",
+      `SELECT 
+  articles.author, 
+  articles.title, 
+  articles.article_id, 
+  articles.topic, 
+  articles.created_at, 
+  articles.votes, 
+  articles.article_img_url, 
+  COUNT(comments.comment_id) AS comment_count
+FROM 
+  articles
+LEFT JOIN 
+  comments ON articles.article_id = comments.article_id
+WHERE 
+  articles.article_id = $1
+GROUP BY 
+  articles.article_id;`,
       value
     )
     .then(({ rows }) => {
