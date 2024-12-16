7e5c0f3bfc8ea748aa6420fb1d7c28b13d05e17c
Oksana Herasymenko <oksanagerasymenko@unknown.home>
2024-12-16 15:19:15 +0000

    add body of article

--- models/app.model.js
+++ models/app.model.js
@@ -12,7 +12,7 @@ exports.readArticleByID = (id) => {
 
   return db
     .query(
-      `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, 
+      `SELECT articles.author, articles.body, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, 
   COUNT(comments.comment_id) AS comment_count
 FROM articles
 LEFT JOIN comments ON articles.article_id = comments.article_id
